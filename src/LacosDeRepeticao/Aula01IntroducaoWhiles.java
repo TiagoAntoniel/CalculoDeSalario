834e258df27635ae7e4bb6332d61b72710e5a36d
Tiago Antoniel <tiagofull7@gmail.com>
2024-09-04 11:21:06 -0300

    Lacos de repeticao utilizando For

--- src/LacosDeRepeticao/Aula01IntroducaoWhiles.java
+++ src/LacosDeRepeticao/Aula01IntroducaoWhiles.java
@@ -2,7 +2,7 @@ package LacosDeRepeticao;
 
 import java.util.Scanner;
 
-public class Aula01Introducao {
+public class Aula01IntroducaoWhiles {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Digite o numero a ser contado: ");
