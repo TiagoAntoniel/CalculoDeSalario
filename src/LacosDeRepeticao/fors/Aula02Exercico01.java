6583782b36cf61abc34b915c13d0f3cf753db568
Tiago Antoniel <tiagofull7@gmail.com>
2024-09-04 11:36:39 -0300

    Aula 02, numeros quadrados de 15 a 200

---
+++ src/LacosDeRepeticao/fors/Aula02Exercico01.java
@@ -0,0 +1,9 @@
+package LacosDeRepeticao.fors;
+
+public class Aula02Exercico01 {
+    public static void main(String[] args) {
+        for (int i = 15; i <=200; i++) {
+            System.out.println(i * i);
+        }
+    }
+}
