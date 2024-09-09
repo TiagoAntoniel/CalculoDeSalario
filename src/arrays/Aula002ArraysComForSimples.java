a3c8cf0b31aaa4b37ce3371f6ab829862a1cdaf1
Tiago Antoniel <tiagofull7@gmail.com>
2024-09-08 21:46:39 -0300

    Exercicio de Arrays (Notas)

--- src/arrays/Aula002ArraysComForSimples.java
+++ src/arrays/Aula002ArraysComForSimples.java
@@ -2,20 +2,17 @@ package Arrays;
 
 import java.util.Scanner;
 
-public class Aula02ArraysComFor {
+public class Aula002ArraysComForSimples {
     public static void main(String[] args) {
         double[] notas = new double[4];
+        double soma = 0;
         Scanner teclado = new Scanner(System.in);
         for (int i = 0; i < notas.length; i++) {
-            System.out.println("Digite a nota: " + (i + 1));
+            System.out.println("Digite a nota: " + (i + 1) + ":");
             notas[i] = teclado.nextDouble();
+            soma += notas[i];
         }
-        double media = 0;
-        for (int i = 0; i < notas.length; i++) {
-            media = media + notas[i];
-            System.out.println("Nota " + (i + 1) + ": " + notas[i]);
-        }
-        media = media / notas.length;
+        double media = soma / notas.length;
         System.out.println("Media das notas: " + media);
     }
 }
